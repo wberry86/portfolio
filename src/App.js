d522391f70c014aabd555a16921336fc92cf7384
William Berry <76190311+wberry86@users.noreply.github.com>
2021-08-19 11:58:42 -0500

    set up navbar and links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+ import React from 'react';
 import logo from './logo.svg';
+// Import the BrowserRouter, Route and Link components
+import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+import Projects from './Projects.js'; 
+import Contact from './Contact.js'; 
+import About from './About.js'; 
 import './App.css';
-
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+  
+        
+        <Route exact path="/" component={Projects} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/about" component={About} />
+  
+        <div className="navigation">
+          <img src={logo} className="logo-image" alt="Logo" />
+          <div className="navigation-sub">
+                                          
+            
+            <Link to="/" className="item">Projects</Link>
+            <Link to="/contact" className="item">Contact</Link>
+            <Link to="/about" className="item">About</Link>
+  
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
-
+ 
 export default App;
